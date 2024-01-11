4830164e78077638497aea23b0726f3dba3ffda1
Veru5 <veronika.pincova5@seznam.cz>
2024-01-11 17:23:35 +0100

    Fourth commit

--- content/pages/formprocess.php
+++ content/pages/formprocess.php
@@ -1,32 +1,5 @@
 <?php 
 
-/* function Conn(){
-    global $conn;
-    // Připojení do databáze
-    $conn = mysqli_connect("localhost", "root", "", "smate");
-}
-
-// Zavolání funkce pro připojení k databázi
-Conn();
-
-// Ověření existence hodnot z $_POST
-$url = isset($_POST['url']) ? $_POST['url'] : '';
-$title = isset($_POST['title']) ? $_POST['title'] : '';
-$meta_description = isset($_POST['meta_description']) ? $_POST['meta_description'] : '';
-
-// Uložení dat do MySQL databáze (tabulky "thetable")
-$sql = "INSERT INTO thetable (url, title, meta_description) VALUES ('$url', '$title', '$meta_description')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Data byla úspěšně uložena do databáze.";
-} else {
-    echo "Chyba při ukládání dat: " . $conn->error;
-}
-
-// Uzavření spojení s databází
-$conn->close(); */
-
-
 function Connection(){
     global $connection;
     // připojení do databáze
